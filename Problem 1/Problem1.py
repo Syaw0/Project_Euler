013d19bc4af3e01373e23eb06df97a3fc2807f6d
siavash <siamhb7@protonmail.com>
2022-05-05 23:53:51 +0430

    frist problem :)

---
+++ Problem 1/Problem1.py
@@ -0,0 +1,9 @@
+def Multiples_Sum():
+    SumAll = 0
+    for Number in range(0,1001):
+        if Number%5 == 0 or Number%3 == 0 :
+            SumAll += Number
+    return print(f"sum Multiples from 0 to 1000 is {SumAll}")
+                                                                #i wnat write a function that take an initial range and end range number and number list and return Multiples of them ... hmmm...
+if __name__ == '__main__':
+    Multiples_Sum()
