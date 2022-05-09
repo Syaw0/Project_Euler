61b28741ab97e1097a95e1c481ae3c5e7d05d036
siavash <siamhb7@protonmail.com>
2022-05-09 20:50:40 +0430

    problem4

--- Problem 4/Problem4.py
+++ Problem 4/Problem4.py
@@ -1,18 +1,14 @@
-def Largest_Palindrome_Product():
-    listnumbers = []
-    for number1 in range(100,1000):
-        for number2 in range(100,1000):
-            res = number1 * number2
-            list = []
-            for char in str(res) :
-                list.append(char)
-            if list[len(list) -1 ] != 0:
-                list.reverse()
-                newnumber = int("".join(list))
-                if newnumber == res:
-                    listnumbers.append(newnumber)
-    return print(max(listnumbers))
-
-            
+def Smallest_Multiple():
+    initNumber = 20
+    while True:
+        for number in range(2 , 21):
+            res = initNumber / number 
+            #print(f"{initNumber} / {number} = {res}")
+            if str(res).split(".")[1] != "0":
+                break
+            elif number == 20 and res % 2 == 0:
+                return print(initNumber)
+        initNumber += 5
+        
 if __name__ == '__main__':
-    Largest_Palindrome_Product()
+    Smallest_Multiple()
